211b4923a91b7ffa98dda5c8468c88f84aa6e3be
Abdelhamed Ahmed <168166326+Eng-abdelhamed@users.noreply.github.com>
2026-07-08 18:10:03 +0300

    Refactor Add and Multiply functions for brevity

--- Code/lib/math.cpp
+++ Code/lib/math.cpp
@@ -1,7 +1,3 @@
 #include "../include/Math.hpp"
-int Add(int a, int b) { 
-        return a + b; 
-}
-int Multiply(int a, int b) { 
-        return a * b; 
-}
+int Add(int a, int b) { return a + b; }
+int Multiply(int a, int b) { return a * b; }
