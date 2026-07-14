788d5337cbd863176b69398decdcab02d032be81
abdelhamewd <abdelhamed>
2026-07-14 17:39:00 +0300

    Updated tensor v2.1

--- Code/include/tensor.h
+++ Code/include/tensor.h
@@ -108,8 +108,7 @@ class Tensor {
     [[nodiscard]] Tensor mean(size_type axis) const;
     [[nodiscard]] value_type max() const;
     [[nodiscard]] value_type min() const;
-    
-  private:
+    private:
     Storage data_;
     Shape shape_;
 
